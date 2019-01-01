5d52c92c75d86f588d6352f628c4692cd7a728d5
christofer bergsten <christoferbergsten@christofers-MacBook-Pro.local>
2019-01-01 12:18:08 +0100

    Render stored images

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const Image = require('../models/Image');
+const getDatabaseRecords = require('./mongo');
 
 router.get('/search', (req, res, next) => {
 res.sendFile(path.join(__dirname, '../../src', 'views', 'search.html'));
+});
+
+router.get('/saved-images', async (req, res, next) => {
+
+    const images = await getDatabaseRecords();
+
+    res.render('saved-images', {recordsImg: images});
+
 });
 
 module.exports = router;
